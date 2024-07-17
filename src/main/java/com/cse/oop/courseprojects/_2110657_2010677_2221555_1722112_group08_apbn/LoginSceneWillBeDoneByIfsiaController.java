f88bb1b056625283b7a7e18c1d9dbecfdf963eb4
Ifsia Sarwar <ifsiasarwar@gmail.com>
2024-07-17 13:53:23 +0600

    First commit

--- src/main/java/com/cse/oop/courseprojects/_2110657_2010677_2221555_1722112_group08_apbn/LoginSceneWillBeDoneByIfsiaController.java
+++ src/main/java/com/cse/oop/courseprojects/_2110657_2010677_2221555_1722112_group08_apbn/LoginSceneWillBeDoneByIfsiaController.java
@@ -1,7 +1,20 @@
 package com.cse.oop.courseprojects._2110657_2010677_2221555_1722112_group08_apbn;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginSceneWillBeDoneByIfsiaController
 {
     @javafx.fxml.FXML
+    private TextField passwordTextFieldLoginScene;
+    @javafx.fxml.FXML
+    private TextField userIDTextFieldLoginScene;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void loginButtonOncClickLoginScene(ActionEvent actionEvent) {
+    }
+}
